21a834a4ded6b28c5e11148aa4b969f6dc4abffa
Zyst <zyst@gmx.com>
2017-01-01 16:01:18 -0600

    Changed hyper fontsize to huge

--- hyper.js
+++ hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 22,
 
     // font family with optional fallbacks
     fontFamily: 'Operator Mono',
