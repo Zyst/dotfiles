bbf15b5ca1d132aab6b396c1b8c23d17d17cce01
Zyst <zyst@gmx.com>
2016-12-31 21:15:09 -0600

    My symlink tests work!

--- hyper.js
+++ hyper.js
@@ -7,7 +7,7 @@ module.exports = {
     fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: 'rgba(50,50,250,0.8)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
