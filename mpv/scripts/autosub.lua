317cc5e07d870eafd8a1d81dc48cb2b90816400b
Zyst <erick.romero.dev@gmail.com>
2017-07-16 22:33:21 -0500

    Updated this one to Windows

--- mpv/scripts/autosub.lua
+++ mpv/scripts/autosub.lua
@@ -4,14 +4,14 @@
 -- keyname script_binding auto_load_subs
 local utils = require 'mp.utils'
 function load_sub_fn()
-    subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
+    subl = "C:\\Python36\\Scripts\\subliminal" -- use 'which subliminal' to find the path
     mp.msg.info("Searching subtitle")
     mp.osd_message("Searching subtitle")
     t = {}
     t.args = {subl, "download", "-s", "-l", "en", mp.get_property("path")}
     res = utils.subprocess(t)
     if res.status == 0 then
-        mp.commandv("rescan_external_files", "reselect") 
+        mp.commandv("rescan_external_files", "reselect")
         mp.msg.info("Subtitle download succeeded")
         mp.osd_message("Subtitle download succeeded")
     else
