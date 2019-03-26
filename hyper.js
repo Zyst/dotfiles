d23be166d3228592d7b8f2eb33618a6fd26eb96d
Erick Romero <erick.romero@aeris.net>
2019-03-25 23:52:44 -0600

    Proper username

--- hyper.js
+++ hyper.js
@@ -60,7 +60,7 @@ module.exports = {
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
-    shell: "C:\\Users\\erick.romero\\go\\bin\\elvish.exe",
+    shell: "C:\\Users\\Zyst\\go\\bin\\elvish.exe",
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
