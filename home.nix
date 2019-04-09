0bf42872d2749311cb3be261b980636680590857
Erick Romero <erick.romero.dev@gmail.com>
2019-04-08 23:45:53 -0500

    Add home.nix

---
+++ home.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "/home/zyst/dev/dotfiles";
+in
+{
+  home = {
+    packages = [
+      pkgs.fortune
+      pkgs.neofetch
+      pkgs.ripgrep
+    ];
+
+    file = {
+      ".config/nvim/init.vim".text =
+      ''
+      set runtimepath^=~/.vim runtimepath+=~/.vim/after
+      let &packpath = &runtimepath
+      source ~/.vimrc
+      '';
+      
+      ".vimrc".source = "${dotfiles}/vimrc";
+
+      ".bashrc".source = "${dotfiles}/bashrc";
+
+      ".tmux.conf".source = "${dotfiles}/tmux.conf";
+    };
+  };
+
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+
+    git = {
+      enable = true;
+      userName = "Erick Romero";
+      userEmail = "erick.romero.dev@gmail.com";
+    };
+
+    neovim = {
+      enable = true;
+    };
+
+    emacs = {
+      enable = true;
+    };
+  };
+}
