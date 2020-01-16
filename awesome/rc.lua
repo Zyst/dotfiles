a3564149bc156c49ccf06f75da225fe33ee2e88f
Erick Romero <erick.romero@bouncex.com>
2020-01-15 21:47:12 -0600

    Re-copy rc.lua from the now global awesome installation

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -18,6 +18,10 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
+-- Load Debian menu entries
+local debian = require("debian.menu")
+local has_fdo, freedesktop = pcall(require, "freedesktop")
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -48,8 +52,8 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
+terminal = "x-terminal-emulator"
+editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
@@ -90,10 +94,24 @@ myawesomemenu = {
   { "quit", function() awesome.quit() end },
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-      { "open terminal", terminal }
-    }
-  })
+local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
+local menu_terminal = { "open terminal", terminal }
+
+if has_fdo then
+  mymainmenu = freedesktop.menu.build({
+      before = { menu_awesome },
+      after =  { menu_terminal }
+    })
+else
+  mymainmenu = awful.menu({
+      items = {
+        menu_awesome,
+        { "Debian", debian.menu.Debian_menu.Debian },
+        menu_terminal,
+      }
+    })
+end
+
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
   menu = mymainmenu })
