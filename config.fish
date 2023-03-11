5334f11517d9e968cabf1347c62f74f206309d1b
Erick Romero <erickromero@fb.com>
2023-03-11 15:38:26 -0500

    Add a some new aliases to fish

--- config.fish
+++ config.fish
@@ -2,6 +2,12 @@ set fish_greeting
 
 alias clrepl="clojure -Sdeps '{:deps {nrepl {:mvn/version \"0.7.0\"} cider/cider-nrepl {:mvn/version \"0.25.0\"}}}' -m nrepl.cmdline --middleware '[\"cider.nrepl/cider-middleware\"]'"
 alias ls="exa --tree --level=1"
+alias juiceme="adb shell ls /sdcard/Oculus/VideoShots/ | sort -r | head -n1 | xargs -I '{}' adb pull /sdcard/Oculus/VideoShots/'{}'"
+alias re="watch -n 1 -g '$1'"
+alias gst="git status"
+alias glg="git log --oneline --decorate --graph"
+alias rgg="rg --files -g"
+alias nz="nvim ~/.zshrc"
 
 function 2fa
   cat /home/zyst/.2fa/$argv | xargs -I {} oathtool --base32 --totp "{}"
